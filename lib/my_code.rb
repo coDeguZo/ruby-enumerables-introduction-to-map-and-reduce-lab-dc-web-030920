af1419bb10f215ce43af579838e46246857ab21a
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-18 22:09:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,18 +53,18 @@ end
 
 def reduce_to_all_true(source_array)
   i = 0
-  while i < source_array.count do
-    return false if source_array[i] === false
-    i += 1
+  while i < source_array.length do
+    return true if source_array[i] === 
+    i +=1
   end
   return true
 end
 
 def reduce_to_any_true(source_array)
   i = 0
-  while i < source_array.count do
+  while i < source_array.length do
     return true if source_array[i] === true
-    i += 1
+    i +=1
   end
   return false
 end
